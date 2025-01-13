39598ea5183dca94798315b2eae32475479cc298
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-01-13 16:10:18 +0200

    feat: footer

---
+++ constants/sections/footer/index.ts
@@ -0,0 +1,36 @@
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "About Hilink",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
+    ],
+  },
+  {
+    title: "Our Community",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "hilink@akinthil.com" },
+  ],
+};
+
+export const FOOTER_SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
